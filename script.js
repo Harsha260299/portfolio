19125861f0a744a42740d8a18b16a830572d56f4
Harsha260299 <160774808+Harsha260299@users.noreply.github.com>
2024-12-18 20:36:20 +0530

    ehanced

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// Fetch and display projects
 fetch('data/projects.json')
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById('projects-container');
-    data.forEach(item => {
-      container.innerHTML += `
-        <div class="project-card">
-          <h3>${item.title}</h3>
-          <p>${item.description}</p>
-          <a href="${item.link}" target="_blank">View Project</a>
-        </div>`;
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('projects-container');
+        data.forEach(project => {
+            container.innerHTML += `
+                <div class="project-card">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+                    <a href="${project.link}" target="_blank">View Project</a>
+                </div>
+            `;
+        });
     });
-  });
 
-// Fetch and display certificates
 fetch('data/certificates.json')
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById('certificates-container');
-    data.forEach(item => {
-      container.innerHTML += `
-        <div class="certificate-card">
-          <h3>${item.name}</h3>
-          <p>Issued by: ${item.issuer}</p>
-          <a href="${item.link}" target="_blank">View Certificate</a>
-        </div>`;
+    .then(response => response.json())
+    .then(data => {
+        const container = document.getElementById('certificates-container');
+        data.forEach(cert => {
+            container.innerHTML += `
+                <div class="certificate-card">
+                    <h3>${cert.name}</h3>
+                    <p>Issued by: ${cert.issuer}</p>
+                    <a href="${cert.link}" target="_blank">View Certificate</a>
+                </div>
+            `;
+        });
     });
-  });
